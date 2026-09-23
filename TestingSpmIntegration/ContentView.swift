2d2d67dac6c1451584eea578dd2d0d2edc494d7d
Simone Gallus <simone.gallus@avanade.com>
2026-09-23 11:51:11 +0200

    Initial Commit

---
+++ TestingSpmIntegration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestingSpmIntegration
+//
+//  Created by Simone Gallus on 23/09/2026.
+//
+
+import SwiftUI
+import MyLibrarySpm
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text(Calculator.sum(lhs: 10, rhs: 20).description)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestingSpmIntegration/TestingSpmIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSpmIntegrationApp.swift
+//  TestingSpmIntegration
+//
+//  Created by Simone Gallus on 23/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestingSpmIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
